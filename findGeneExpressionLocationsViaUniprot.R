91d96f8658e9a02b9d35776466f42274b4aea99b
San-Yuan Wang <sanyuan.wang@gmail.com>
2018-04-27 12:12:56 +0800

    add a R function for finding gene expression location via Uniprot

---
+++ findGeneExpressionLocationsViaUniprot.R
@@ -0,0 +1,26 @@
+findGeneExpressionLocationsViaUniprot = function(GeneNames = c("CD180"), SleepTime = 0)
+{
+  TISSUELocation = list()
+  for(cGeneName in GeneNames)
+  {
+    url = paste0("https://www.uniprot.org/uniprot/?query=", cGeneName,"&format=tab")
+    QueryResult = read.table(url, header = T, fill = NA, sep = "\t", stringsAsFactors = F)
+    EntryIDs = QueryResult[, "Entry"]
+    TISSUELocation[[cGeneName]] = list()
+    for(cEntryID in EntryIDs)
+    {
+      print(paste0(cGeneName," ", cEntryID, " ", which(EntryID == cEntryID), "/", length(EntryID)))
+      url = paste0("https://www.uniprot.org/uniprot/", cEntryID,".txt") 
+      txt = readLines(url)
+      txt = txt[grep("TISSUE=", txt)]
+      if(length(txt) > 0)
+      {
+        TISSUELocation[[cGeneName]] = rbind(TISSUELocation[[cGeneName]], 
+                                            cbind(cEntryID, Location = gsub(".*TISSUE=|;$| \\{.*\\}|\\{.*,", "", txt)))
+      }
+      Sys.sleep(SleepTime)
+    }
+    #TISSUELocation[[cGeneName]] = unique(as.character(TISSUELocation[[cGeneName]][, "Location"]))
+  }
+  TISSUELocation
+}
